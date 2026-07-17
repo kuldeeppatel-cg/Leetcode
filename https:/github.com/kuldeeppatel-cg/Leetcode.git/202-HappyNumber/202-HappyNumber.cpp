107cc0a975bbfc067d7f394d70a94428a763e7ab
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-07-17 14:19:57 +0530

    Create 202-HappyNumber

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/202-HappyNumber/202-HappyNumber.cpp
@@ -0,0 +1,29 @@
+// Last updated: 17/07/2026, 14:19:56
+class Solution {
+public:
+
+    int digSqr(int num){
+        int res = 0;
+        while(num > 0){
+            int dig = num%10;
+            res += dig * dig;
+            num /= 10;
+        }
+        return res;
+    }
+
+    bool isHappy(int n) {
+       unordered_map<int,int> check;
+        while(n != 1 ){
+            if(check[n]==2){
+                break;
+            }
+           n = digSqr(n);
+           check[n]++;
+        }
+        if(n == 1){
+            return true;
+        }
+        return false;
+    }
+};
