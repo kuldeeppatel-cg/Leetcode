76ad506780d585cc630f49c78e7adb624e40ced1
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-07-17 14:19:17 +0530

    Create 953-ReverseOnlyLetters

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/953-ReverseOnlyLetters/953-ReverseOnlyLetters.cpp
@@ -0,0 +1,22 @@
+// Last updated: 17/07/2026, 14:19:17
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (isalpha(s[i]) && isalpha(s[j])) {
+                swap(s[i], s[j]);
+                i++;
+                j--;
+            } else {
+                if (!isalpha(s[i])) {
+                    i++;
+                } else {
+                    j--;
+                }
+            }
+        }
+        return s;
+    }
+};
