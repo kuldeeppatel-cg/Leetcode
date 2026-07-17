09da52e3eb253290cd2d0f0ee2aa6662bc14563b
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-07-17 14:20:10 +0530

    Create 71-SimplifyPath

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/71-SimplifyPath/71-SimplifyPath.cpp
@@ -0,0 +1,30 @@
+// Last updated: 17/07/2026, 14:20:09
+class Solution {
+public:
+    string
+    simplifyPath(string path) { // path = ""/home/user/Documents/../Pictures""
+        vector<string> st;
+        string curr = ""; // curr = ""        st = {"home","user","Pictures"}
+
+        for (int i = 0; i <= path.size(); i++) {
+            if (i == path.size() || path[i] == '/') { // path[i] = /
+                if (curr == "..") {
+                    if (!st.empty())
+                        st.pop_back();
+                } else if (!curr.empty() && curr != ".") {
+                    st.push_back(curr);
+                }
+                curr = "";
+            } else {
+                curr += path[i];
+            }
+        }
+
+        string ans = "";
+        for (string dir : st) { //  ans = "/home/user/Pictures"
+            ans += "/" + dir;
+        }
+
+        return ans.empty() ? "/" : ans;
+    }
+};
