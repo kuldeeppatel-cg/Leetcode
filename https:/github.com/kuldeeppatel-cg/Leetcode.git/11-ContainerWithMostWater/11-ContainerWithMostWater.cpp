fcd02c435904d1f3e3ee6f19b843451812b78c50
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-07-17 14:20:28 +0530

    Create 11-ContainerWithMostWater

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
@@ -0,0 +1,23 @@
+// Last updated: 17/07/2026, 14:20:27
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left =0;
+        int right = height.size() -1;
+        int ans = 0;
+
+        while(left <= right){
+             int h = min(height[left],height[right]);
+            int w = right - left;
+
+            ans = max(ans,h * w); 
+            if(height[left] < height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return ans;
+    }
+};
