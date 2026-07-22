d3c97fd8611703af588130e71a474450c3cda84f
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-07-22 09:13:53 +0530

    Create 53-MaximumSubarray

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/53-MaximumSubarray/53-MaximumSubarray.cpp
@@ -0,0 +1,15 @@
+// Last updated: 22/07/2026, 09:13:53
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            currentSum = max(nums[i], currentSum + nums[i]);
+            maxSum = max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+};
