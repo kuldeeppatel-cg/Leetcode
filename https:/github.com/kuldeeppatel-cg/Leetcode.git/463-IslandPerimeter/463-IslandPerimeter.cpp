b84b49946220f1194afe53830f3da48cb5396ab7
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-08-02 18:58:22 +0530

    Create 463-IslandPerimeter

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/463-IslandPerimeter/463-IslandPerimeter.cpp
@@ -0,0 +1,25 @@
+// Last updated: 02/08/2026, 18:58:22
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int perimeter = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+
+                    if (i + 1 < m && grid[i + 1][j] == 1)
+                        perimeter -= 2;
+
+                    if (j + 1 < n && grid[i][j + 1] == 1)
+                        perimeter -= 2;
+                }
+            }
+        }
+
+        return perimeter;
+    }
+};
