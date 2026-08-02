21b0fcf72a2694613a84e1a5534a739f4dd69fa4
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-08-02 18:58:50 +0530

    Create 349-IntersectionOfTwoArrays

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/349-IntersectionOfTwoArrays/349-IntersectionOfTwoArrays.cpp
@@ -0,0 +1,16 @@
+// Last updated: 02/08/2026, 18:58:50
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> s(nums1.begin(), nums1.end());
+        unordered_set<int> result;
+
+        for (int num : nums2) {
+            if (s.count(num)) {
+                result.insert(num);
+            }
+        }
+
+        return vector<int>(result.begin(), result.end());
+    }
+};
