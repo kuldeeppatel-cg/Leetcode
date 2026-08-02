d868c3762e4cdcbe19bd4c5842ac4796b26ebeb4
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-08-02 18:58:35 +0530

    Create 404-SumOfLeftLeaves

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/404-SumOfLeftLeaves/404-SumOfLeftLeaves.cpp
@@ -0,0 +1,21 @@
+// Last updated: 02/08/2026, 18:58:35
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+
+        int sum = 0;
+
+        if (root->left &&
+            root->left->left == nullptr &&
+            root->left->right == nullptr) {
+            sum += root->left->val;
+        }
+
+        sum += sumOfLeftLeaves(root->left);
+        sum += sumOfLeftLeaves(root->right);
+
+        return sum;
+    }
+};
