3d21e61c1a94db7ef34672a391a5912577c7e5b0
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-08-12 14:04:46 +0530

    Create 29-DivideTwoIntegers

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/29-DivideTwoIntegers/29-DivideTwoIntegers.cpp
@@ -0,0 +1,24 @@
+// Last updated: 12/08/2026, 14:04:46
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
+
+        long long a = abs((long long)dividend);
+        long long b = abs((long long)divisor);
+        long long ans = 0;
+
+        for (int i = 31; i >= 0; i--) {
+            if ((b << i) <= a) {
+                a -= (b << i);
+                ans += (1LL << i);
+            }
+        }
+
+        if ((dividend < 0) ^ (divisor < 0))
+            ans = -ans;
+
+        return (int)ans;
+    }
+};
