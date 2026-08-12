51c88a488359f56521e284f9bbeaa6f6189a71ee
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-08-12 14:02:25 +0530

    Create 830-LargestTriangleArea

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/830-LargestTriangleArea/830-LargestTriangleArea.cpp
@@ -0,0 +1,26 @@
+// Last updated: 12/08/2026, 14:02:26
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& points) {
+        double ans = 0;
+
+        int n = points.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+
+                    double area = abs(
+                        points[i][0] * (points[j][1] - points[k][1]) +
+                        points[j][0] * (points[k][1] - points[i][1]) +
+                        points[k][0] * (points[i][1] - points[j][1])
+                    ) / 2.0;
+
+                    ans = max(ans, area);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
