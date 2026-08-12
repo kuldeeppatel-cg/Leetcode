490829eecc8564e14f125aca99f9d59340372fdd
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-08-12 14:03:54 +0530

    Create 206-ReverseLinkedList

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/206-ReverseLinkedList/206-ReverseLinkedList.cpp
@@ -0,0 +1,27 @@
+// Last updated: 12/08/2026, 14:03:55
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* current = head;
+        ListNode* prev = nullptr;
+
+        while (current != nullptr) {
+            ListNode* nextptr = current->next; 
+            current->next = prev;             
+            prev = current;                    
+            current = nextptr;                 
+        }
+
+        return prev;
+    }
+};
