5175aabbe44328ff20fc9ed9334d68b07d225bb6
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-08-12 14:02:42 +0530

    Create 485-MaxConsecutiveOnes

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.cpp
@@ -0,0 +1,19 @@
+// Last updated: 12/08/2026, 14:02:43
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0;
+        int maxi = 0;
+
+        for (int num : nums) {
+            if (num == 1) {
+                count++;
+                maxi = max(maxi, count);
+            } else {
+                count = 0;
+            }
+        }
+
+        return maxi;
+    }
+};
