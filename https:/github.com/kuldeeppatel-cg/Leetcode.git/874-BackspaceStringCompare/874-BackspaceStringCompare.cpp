f33c1d05edfdc7589ab3884c09f99c76ee9139f7
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-08-12 14:02:14 +0530

    Create 874-BackspaceStringCompare

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/874-BackspaceStringCompare/874-BackspaceStringCompare.cpp
@@ -0,0 +1,57 @@
+// Last updated: 12/08/2026, 14:02:14
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int i = s.size() - 1;
+        int j = t.size() - 1;
+
+        int skipS = 0;
+        int skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+
+            // Find next valid character in s
+            while (i >= 0) {
+                if (s[i] == '#') {
+                    skipS++;
+                    i--;
+                }
+                else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                }
+                else {
+                    break;
+                }
+            }
+
+            // Find next valid character in t
+            while (j >= 0) {
+                if (t[j] == '#') {
+                    skipT++;
+                    j--;
+                }
+                else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                }
+                else {
+                    break;
+                }
+            }
+
+            // Compare valid characters
+            if (i >= 0 && j >= 0 && s[i] != t[j])
+                return false;
+
+            // One has a character, other doesn't
+            if ((i >= 0) != (j >= 0))
+                return false;
+
+            i--;
+            j--;
+        }
+
+        return true;
+    }
+};
