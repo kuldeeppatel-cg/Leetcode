2300bf84407506ba66a007fdb41a6152f8b3582f
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-08-22 23:39:29 +0530

    Create 1115-ValidBoomerang

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/1115-ValidBoomerang/1115-ValidBoomerang.cpp
@@ -0,0 +1,17 @@
+// Last updated: 22/08/2026, 23:39:29
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>>& points) {
+        int x1 = points[0][0];
+        int y1 = points[0][1];
+
+        int x2 = points[1][0];
+        int y2 = points[1][1];
+
+        int x3 = points[2][0];
+        int y3 = points[2][1];
+
+        return (x2 - x1) * (y3 - y1) !=
+               (y2 - y1) * (x3 - x1);
+    }
+};
