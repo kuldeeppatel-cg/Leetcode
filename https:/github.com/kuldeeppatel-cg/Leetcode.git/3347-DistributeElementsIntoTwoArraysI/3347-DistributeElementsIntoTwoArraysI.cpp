65ca5973dab2a231824340fb1349aeba59ced9ae
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-08-22 23:37:47 +0530

    Create 3347-DistributeElementsIntoTwoArraysI

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/3347-DistributeElementsIntoTwoArraysI/3347-DistributeElementsIntoTwoArraysI.cpp
@@ -0,0 +1,21 @@
+// Last updated: 22/08/2026, 23:37:46
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int> a, b;
+
+        a.push_back(nums[0]);
+        b.push_back(nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+            if (a.back() > b.back())
+                a.push_back(nums[i]);
+            else
+                b.push_back(nums[i]);
+        }
+
+        a.insert(a.end(), b.begin(), b.end());
+
+        return a;
+    }
+};
