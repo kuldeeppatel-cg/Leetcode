c27d813fe5506e4ed2d00695cc98d1068a91a9cc
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-09-07 13:42:36 +0530

    Create 2469-ConvertTheTemperature

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/2469-ConvertTheTemperature/2469-ConvertTheTemperature.cpp
@@ -0,0 +1,14 @@
+// Last updated: 07/09/2026, 13:42:37
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        vector<double> ans;
+        double kelvin = celsius + 273.15;
+        double Fahrenheit = celsius*1.80 + 32.00;
+
+        ans.push_back(kelvin);
+        ans.push_back(Fahrenheit);
+
+        return ans;
+    }
+};
