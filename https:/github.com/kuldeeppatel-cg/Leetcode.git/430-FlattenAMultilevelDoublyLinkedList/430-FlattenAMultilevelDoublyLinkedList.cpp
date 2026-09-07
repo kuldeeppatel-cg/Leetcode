d8e361ff951922890a493e0e416a8aec3ca3ffbf
Kuldeep Patel <kuldeep.patel.cg@gmail.com>
2026-09-07 13:47:31 +0530

    Create 430-FlattenAMultilevelDoublyLinkedList

---
+++ https:/github.com/kuldeeppatel-cg/Leetcode.git/430-FlattenAMultilevelDoublyLinkedList/430-FlattenAMultilevelDoublyLinkedList.cpp
@@ -0,0 +1,52 @@
+// Last updated: 07/09/2026, 13:47:30
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        if (head == nullptr)
+            return head;
+
+        Node* curr = head;
+
+        while (curr) {
+            if (curr->child) {
+                Node* next = curr->next;
+
+                // Flatten child list
+                Node* child = flatten(curr->child);
+
+                // Connect current node to child
+                curr->next = child;
+                child->prev = curr;
+                curr->child = nullptr;
+
+                // Find the end of child list
+                Node* tail = child;
+
+                while (tail->next) {
+                    tail = tail->next;
+                }
+
+                // Connect child list to original next
+                tail->next = next;
+
+                if (next)
+                    next->prev = tail;
+            }
+
+            curr = curr->next;
+        }
+
+        return head;
+    }
+};
